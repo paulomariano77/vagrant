9b5019365f86f70a67d585d27e45989bb9d2c7ad
Paulo Mariano <pazevedo@rj_dev0099.stone.local>
2016-12-19 19:05:51 -0200

    Provision vm_ubuntu with ansible

--- vm_ubunut/webserver.sh
+++ vm_ubunut/webserver.sh
@@ -2,9 +2,19 @@
 
 echo "Update repositorys"
 sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
 
-echo "Install the nginx"
-sudo apt-get install -y nginx
+#echo "Install the nginx"
+#sudo apt-get install -y nginx
 
-echo "Install the PHP"
-sudo apt-get install -y php5-fpm
+#echo "Install the PHP"
+#sudo apt-get install -y php5-fpm
+
+echo "Install ansible"
+sudo apt-get -y install ansible
+
+echo "Install and configure git"
+sudo apt-get -y install git-all
+cd ~/
+mkdir git
